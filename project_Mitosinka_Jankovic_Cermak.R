c5c5485c5020160b1193b0378c2bb6df6e764054
peparyga <josef.mitosinka@student.gyarab.cz>
2026-04-27 14:58:47 +0200

    cleaning session

--- project_Mitosinka_Jankovic_Cermak.R
+++ project_Mitosinka_Jankovic_Cermak.R
@@ -44,3 +44,54 @@ hpi_clean <- hpi_data %>%
 head(labour_costs_clean)
 head(hicp_clean)
 head(hpi_clean)
+
+# 3.1 Get a list of all unique countries from each dataset
+countries_lc <- unique(labour_costs_clean$country)
+countries_hicp <- unique(hicp_clean$country)
+countries_hpi <- unique(hpi_clean$country)
+
+common_countries <- intersect(intersect(countries_lc, countries_hicp), countries_hpi)
+print(paste("Number of common countries:", length(common_countries)))
+print(common_countries)
+
+
+# 3.3 Delete the excess countries from our datasets
+labour_costs_filtered <- labour_costs_clean %>%
+  filter(country %in% common_countries)
+
+hicp_filtered <- hicp_clean %>%
+  filter(country %in% common_countries)
+
+hpi_filtered <- hpi_clean %>%
+  filter(country %in% common_countries)
+
+
+unique(hpi_filtered$purchase)
+unique(hpi_filtered$unit)
+
+unique(labour_costs_filtered$industry)
+unique(labour_costs_filtered$cost_type)
+
+# --- 4. FINÁLNÍ FILTROVÁNÍ KATEGORIÍ ---
+
+# 4.1 HPI: Chceme jen celkové nákupy (Total) a roční změnu
+hpi_final <- hpi_filtered %>%
+  filter(purchase == "Total", 
+         unit == "Annual average rate of change") # nebo "Annual average index, 2015=100"
+
+# 4.2 Labour: Chceme celou ekonomiku, jen platy a roční změnu
+labour_final <- labour_costs_filtered %>%
+  filter(industry %in% c("B-S", "Industry, construction and services (except activities of households as employers and extra-territorial organisations and bodies)"), 
+         cost_type %in% c("D11", "Wages and salaries (total)"), 
+         unit == "Percentage change on previous period")
+
+# 4.3 HICP: Jen kontrola, abychom měli stejnou jednotku
+hicp_final <- hicp_filtered %>%
+  filter(unit == "Annual average rate of change") 
+
+# Kontrola, zda máme všude zhruba podobný počet řádků!
+nrow(hpi_final)
+nrow(labour_final)
+nrow(hicp_final)
+
+setdiff(countries_hpi, common_countries)
